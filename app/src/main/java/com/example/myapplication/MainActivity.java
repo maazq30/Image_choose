749178131834e1cc6c11ec8c054797c320345f67
mz <maazqureshi006@gmail.com>
2024-06-28 14:46:49 +0530

    added circular features and crop does not work

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,46 +1,51 @@
 package com.example.myapplication;
 
-import static android.content.ContentValues.TAG;
 
-import android.annotation.SuppressLint;
+
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.database.Cursor;
+
 import android.graphics.Bitmap;
 import android.net.Uri;
+
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
+
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.graphics.drawable.RoundedBitmapDrawable;
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
     ImageView imageview;
+    final int PIC_CROP = 1;
     String filename_text;
 ActivityResultLauncher<Intent> resultlauncher;
+    private Bundle savedInstanceState;
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PIC_CROP) {
+            if (data != null) {
+
+                Bundle extras = data.getExtras();
+                Bitmap selectedBitmap = extras.getParcelable("data");
+
+
+                imageview.setImageBitmap(selectedBitmap);
+            }
+        }
     }
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,35 +56,69 @@ ActivityResultLauncher<Intent> resultlauncher;
         registerResult();
         btn.setOnClickListener(view ->pickimage());
 
-
-
-    }
-    private void pickimage(){
-        Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
-        resultlauncher.launch(intent);
     }
 
+            private void pickimage () {
+                Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+                resultlauncher.launch(intent);
 
 
-    private void registerResult(){
-        resultlauncher=registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
+            }
 
-                new ActivityResultCallback <ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result){
-                        try {
-                            Uri uri = result.getData().getData();
-                            imageview.setImageURI(uri);
 
-                        }catch (Exception e){
-                            Toast.makeText(MainActivity.this, "Pic" , Toast.LENGTH_SHORT).show();
+            private void registerResult() {
+                resultlauncher = registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+
+                        new ActivityResultCallback<ActivityResult>() {
+                            @Override
+                            public void onActivityResult(ActivityResult result) {
+                                try {
+
+                                    Uri uri = result.getData().getData();
+
+
+                                    imageview.setImageURI(uri);
+                                    //performCrop(uri );
+                                } catch (Exception e) {
+                                    Toast.makeText(MainActivity.this, "Pic", Toast.LENGTH_SHORT).show();
+                                }
+                            }
                         }
-                    }
-                }
-        );
-    }
 
+                );
+
+            }
+            /*private void performCrop (Uri Uri){
+        try {
+                    Intent cropIntent = new Intent("com.example.myapplication");
+                    // indicate image type and Uri
+                    cropIntent.setDataAndType(Uri, "image/*");
+                    // set crop properties here
+                    cropIntent.putExtra("crop", true);
+                    // indicate aspect of desired crop
+                    cropIntent.putExtra("aspectX", 1);
+                    cropIntent.putExtra("aspectY", 1);
+                    // indicate output X and Y
+                    cropIntent.putExtra("outputX", 128);
+                    cropIntent.putExtra("outputY", 128);
+                    // retrieve data on return
+                    cropIntent.putExtra("return-data", true);
+                    // start the activity - we handle returning in onActivityResult
+                    startActivityForResult(cropIntent, PIC_CROP);
+                }
+                // respond to users whose devices do not support the crop action
+                catch (ActivityNotFoundException anfe) {
+
+                    String errorMessage = " your device doesn't support the crop action!";
+                    Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+
+            }*/
 
 
 }
+
+
